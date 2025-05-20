3a2b47e9deab3f354430131356989958e110f9c9
Ingemar Berghult <ingemar.kjellman@gmail.com>
2025-05-20 14:55:38 +0200

    added first functionality

--- public/client.js
+++ public/client.js
@@ -6,6 +6,7 @@ const inlogForm = document.querySelector("#inlogForm");
 const gameForm = document.querySelector("#gameForm");
 
 let player;
+let handsPlayed = 0;
 
 inlogForm.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -13,4 +14,18 @@ inlogForm.addEventListener("submit", function (e) {
   header.innerHTML = `<h4>Welcome ${player}. Let's play som Rock Scissors Paper</h4>`;
   document.getElementById("inlogView").style.display = "none";
   document.getElementById("gameView").style.display = "block";
+});
+
+gameForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let hand;
+  let handShape = document.getElementsByName("handShape");
+  for (i = 0; i < handShape.length; i++) {
+    if (handShape[i].checked) hand = handShape[i].value;
+  }
+  socket.emit("play", {
+    player: player,
+    play: hand,
+  });
+  console.log(hand);
 });
