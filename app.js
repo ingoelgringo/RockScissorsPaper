d95cfcc50a2a60f16217e10dbd19b4791c0a932b
Ingemar Berghult <ingemar.kjellman@gmail.com>
2025-05-22 14:36:05 +0200

    add functional game mechanic

--- app.js
+++ app.js
@@ -15,6 +15,81 @@ connectionMongoDB();
 
 app.use(express.static("public"));
 
+let players = [];
+
+io.on("connection", (socket) => {
+  console.log(`A client with id ${socket.id} connected to the game`);
+
+  socket.on("login", (playerName) => {
+    players.push({
+      playerId: socket.id,
+      playerName: playerName,
+      handPlayed: null,
+      wins: 0,
+      losses: 0,
+    });
+  });
+
+  // funktionalitet för att skicka svar när båda spelarna gjort sitt val
+  socket.on("showHand", (hand) => {
+    function findPlayer(obj) {
+      return obj.playerId === socket.id;
+    }
+    let index = players.findIndex(findPlayer);
+    players[index].handPlayed = hand;
+    console.log(players);
+
+    if (players[0].handPlayed && players[1].handPlayed) {
+      let winner = checkWinner(players);
+      console.log("Winner: ", winner);
+      io.emit("result", winner);
+      players.forEach((player) => (player.handPlayed = null));
+      console.log("After hands are nulled: ", players);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`Client ${socket.id} left the game`);
+    // delete playersId[socket.id];
+    // console.log(playersId);
+  });
+});
+
 server.listen(port, () => {
   console.log(`Socket.IO server is running at http:localhost:${port}/`);
 });
+
+function checkWinner(players) {
+  let p1 = players[0];
+  let p2 = players[1];
+  let outcome;
+
+  if (p1.handPlayed === p2.handPlayed) {
+    outcome = [
+      p1.playerName + " played " + p1.handPlayed,
+      p2.playerName + " played " + p2.handPlayed,
+      "It's a draw",
+    ];
+  } else if (
+    (p1.handPlayed === "paper" && p2.handPlayed === "rock") ||
+    (p1.handPlayed === "rock" && p2.handPlayed === "scissors") ||
+    (p1.handPlayed === "scissors" && p2.handPlayed === "paper")
+  ) {
+    outcome = [
+      p1.playerName + " played " + p1.handPlayed,
+      p2.playerName + " played " + p2.handPlayed,
+      p1.playerName + " wins this round",
+    ];
+    players[0].wins++;
+    players[1].losses++;
+  } else {
+    outcome = [
+      p1.playerName + " played " + p1.handPlayed,
+      p2.playerName + " played " + p2.handPlayed,
+      p2.playerName + " wins this round",
+    ];
+    players[1].wins++;
+    players[0].losses++;
+  }
+  return outcome;
+}
--- public/client.js
+++ public/client.js
@@ -4,9 +4,10 @@ const header = document.querySelector("#header");
 const playerName = document.querySelector("#playerName");
 const inlogForm = document.querySelector("#inlogForm");
 const gameForm = document.querySelector("#gameForm");
+const result = document.querySelector("#result");
+const playBtn = document.querySelector("#playBtn");
 
 let player;
-let handsPlayed = 0;
 
 inlogForm.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -14,6 +15,7 @@ inlogForm.addEventListener("submit", function (e) {
   header.innerHTML = `<h4>Welcome ${player}. Let's play som Rock Scissors Paper</h4>`;
   document.getElementById("inlogView").style.display = "none";
   document.getElementById("gameView").style.display = "block";
+  socket.emit("login", player);
 });
 
 gameForm.addEventListener("submit", function (e) {
@@ -23,9 +25,20 @@ gameForm.addEventListener("submit", function (e) {
   for (i = 0; i < handShape.length; i++) {
     if (handShape[i].checked) hand = handShape[i].value;
   }
-  socket.emit("play", {
-    player: player,
-    play: hand,
-  });
-  console.log(hand);
+  socket.emit("showHand", hand);
+  playBtn.disabled = true;
+  // result.innerHTML = "<p>" + player + " played " + hand + "</p>";
+});
+
+socket.on("result", function (winner) {
+  let p1 = document.createElement("p");
+  let p2 = document.createElement("p");
+  let p3 = document.createElement("p");
+  p1.textContent = winner[0];
+  p2.textContent = winner[1];
+  p3.textContent = winner[2];
+  result.insertBefore(p3, result.firstChild);
+  result.insertBefore(p2, result.firstChild);
+  result.insertBefore(p1, result.firstChild);
+  playBtn.disabled = false;
 });
